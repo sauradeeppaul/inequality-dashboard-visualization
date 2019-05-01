4df6e7d1b8db4954f9c1e4e107c46d887f84b65f
Sauradeep Paul <sauradeep.paul@gmail.com>
2019-05-01 17:03:58 -0400

    New logo; removing old methods

--- blocks_v4.js
+++ blocks_v4.js
@@ -129,7 +129,7 @@ function render_map_plot_v2(){
   function ready(error, country_features, data) {
     if (error) throw error; 
 
-    var gdp_column = 'val';
+    var data_column = 'val';
 
     var populationByCountry = {};
 
@@ -145,11 +145,11 @@ function render_map_plot_v2(){
       minYear = Math.min(minYear, parseInt(d['Year']));
       maxYear = Math.max(maxYear, parseInt(d['Year']));
 
-      populationByCountry[d['Year']][d['Code']] = d[gdp_column]; 
+      populationByCountry[d['Year']][d['Code']] = d[data_column]; 
 
       if(d['Year'] == current_year){
-        minVal = Math.min(minVal, parseInt(d[gdp_column]));
-        maxVal = Math.max(maxVal, parseInt(d[gdp_column]));
+        minVal = Math.min(minVal, parseInt(d[data_column]));
+        maxVal = Math.max(maxVal, parseInt(d[data_column]));
       }});
 
 
@@ -226,89 +226,3 @@ function render_map_plot_v2(){
 
   }
 }
-
-
-function render_map_plot() {
-  var format = d3.format(",");
-
-  // Set tooltips
-  var tip = d3.tip()
-              .attr('class', 'd3-tip')
-              .offset([-10, 0])
-              .html(function(d) {
-                return "<strong>Country: </strong><span class='details'>" + d.properties.name + "<br></span>" + "<strong>Population: </strong><span class='details'>" + format(d.population) +"</span>";
-              })
-
-  var color = d3.scaleThreshold()
-      .domain([10000,100000,500000,1000000,5000000,10000000,50000000,100000000,500000000,1500000000])
-      .range(["rgb(247,251,255)", "rgb(222,235,247)", "rgb(198,219,239)", "rgb(158,202,225)", "rgb(107,174,214)", "rgb(66,146,198)","rgb(33,113,181)","rgb(8,81,156)","rgb(8,48,107)","rgb(3,19,43)"]);
-
-  var path = d3.geoPath();
-
-  var svg = d3.select("svg")
-              .attr("width", width)
-              .attr("height", height)
-              .append('g')
-              .attr('class', 'map');
-
-  var projection = d3.geoMercator()
-                     .scale(130)
-                    .translate( [width / 2, height / 1.5]);
-
-  var path = d3.geoPath().projection(projection);
-
-  svg.call(tip);
-
-  queue()
-      .defer(d3.json, "https://gist.githubusercontent.com/micahstubbs/8e15870eb432a21f0bc4d3d527b2d14f/raw/a45e8709648cafbbf01c78c76dfa53e31087e713/world_countries.json")
-      .defer(d3.tsv, "https://gist.githubusercontent.com/micahstubbs/8e15870eb432a21f0bc4d3d527b2d14f/raw/a45e8709648cafbbf01c78c76dfa53e31087e713/world_population.tsv")
-      .await(ready);
-
-  function ready(error, data, population) {
-    var populationById = {};
-
-    console.log(population)
-    console.log(data.features)
-
-    population.forEach(function(d) { populationById[d.id] = +d.population; });
-    data.features.forEach(function(d) { d.population = populationById[d.id] });
-
-    svg.append("g")
-        .attr("class", "countries")
-      .selectAll("path")
-        .data(data.features)
-      .enter().append("path")
-        .attr("d", path)
-        .style("fill", function(d) { 
-          return color(populationById[d.id]); })
-        .style('stroke', 'white')
-        .style('stroke-width', 1.5)
-        .style("opacity",0.8)
-        // tooltips
-          .style("stroke","white")
-          .style('stroke-width', 0.3)
-          .on('mouseover',function(d){
-            tip.show(d);
-
-            d3.select(this)
-              .style("opacity", 1)
-              .style("stroke","white")
-              .style("stroke-width",3);
-          })
-          .on('mouseout', function(d){
-            tip.hide(d);
-
-            d3.select(this)
-              .style("opacity", 0.8)
-              .style("stroke","white")
-              .style("stroke-width",0.3);
-          });
-
-    svg.append("path")
-        .datum(topojson.mesh(data.features, function(a, b) { return a.id !== b.id; }))
-         // .datum(topojson.mesh(data.features, function(a, b) { return a !== b; }))
-        .attr("class", "names")
-        .attr("d", path);
-  }
-}
-
