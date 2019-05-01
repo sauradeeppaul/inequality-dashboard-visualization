018fd715b40e1dc20311e11c2d6f5720e1c4d995
Sauradeep Paul <sauradeep.paul@gmail.com>
2019-05-01 19:11:35 -0400

    Modal for sim and pca plots

--- blocks_v4.js
+++ blocks_v4.js
@@ -22,7 +22,9 @@ function initialize(){
 
   current_feature = map_features[0];
 
-  prepareDropdown();
+  prepare_dropdown();
+
+  prepare_modal();
 
   svg = d3.select("svg")
             .attr("width", width)
@@ -249,7 +251,7 @@ function render_map_plot_v2(){
 }
 
 
-function prepareDropdown() {
+function prepare_dropdown() {
   console.log("F:prepareDropdown()")
 
   var dropdownChange = function () {
@@ -275,4 +277,43 @@ function prepareDropdown() {
     .text(function (d) {
       return d[0].toUpperCase() + d.slice(1, d.length);
     });
+}
+
+
+function prepare_modal(){
+  // Get the modal
+  var modal = document.getElementById('myModal');
+
+  // Get the button that opens the modal
+  var pca_btn = document.getElementById("pca_button");
+  var sim_btn = document.getElementById("sim_button");
+
+  // Get the <span> element that closes the modal
+  // var span = document.getElementById("close");
+
+  // console.log(span)
+
+  // When the user clicks on the button, open the modal 
+  pca_btn.onclick = function() {
+    console.log("PCA")
+    modal.style.display = "block";
+  }
+
+  sim_btn.onclick = function() {
+    console.log("Similarity")
+    modal.style.display = "block";
+  }
+
+
+  // When the user clicks on <span> (x), close the modal
+  // span.onclick = function() {
+  //   modal.style.display = "none";
+  // }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
 }
